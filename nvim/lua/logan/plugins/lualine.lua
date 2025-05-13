f86b879c7fd41dd4bffc961adee54d8d4cd23c02
logan <loganschaffee@gmail.com>
2025-05-13 08:32:51 -0700

    save

--- nvim/lua/logan/plugins/lualine.lua
+++ nvim/lua/logan/plugins/lualine.lua
@@ -7,14 +7,13 @@ return {
 		"lunarvim/darkplus.nvim",
 	},
 	config = function()
-		-- Modify darkplus theme to give lualine transparent background
 		local custom_darkplus = require("lualine.themes.darkplus")
+
 		custom_darkplus.normal.c.bg = nil
 
 		require("lualine").setup({
 			options = {
 				theme = custom_darkplus,
-				section_separators = { left = "", right = "" },
 			},
 			extensions = { "nvim-tree" },
 		})
