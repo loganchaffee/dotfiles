08d62f978543b4ec1ee377c98728810c79e251ae
logan <loganschaffee@gmail.com>
2025-05-14 08:41:56 -0700

    save

--- nvim/lua/logan/plugins/formatter.lua
+++ nvim/lua/logan/plugins/formatter.lua
@@ -1,30 +1,31 @@
-return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				c = { "clang-format" },
-				go = { "goimports", "gofmt" },
-			},
-			format_on_save = {
-				lsp_fallback = false,
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-	end,
-}
+return {}
+-- return {
+-- 	"stevearc/conform.nvim",
+-- 	event = { "BufReadPre", "BufNewFile" },
+-- 	config = function()
+-- 		require("conform").setup({
+-- 			formatters_by_ft = {
+-- 				javascript = { "prettier" },
+-- 				typescript = { "prettier" },
+-- 				javascriptreact = { "prettier" },
+-- 				typescriptreact = { "prettier" },
+-- 				svelte = { "prettier" },
+-- 				css = { "prettier" },
+-- 				html = { "prettier" },
+-- 				json = { "prettier" },
+-- 				yaml = { "prettier" },
+-- 				markdown = { "prettier" },
+-- 				graphql = { "prettier" },
+-- 				lua = { "stylua" },
+-- 				python = { "isort", "black" },
+-- 				c = { "clang-format" },
+-- 				go = { "goimports", "gofmt" },
+-- 			},
+-- 			format_on_save = {
+-- 				lsp_fallback = false,
+-- 				async = false,
+-- 				timeout_ms = 500,
+-- 			},
+-- 		})
+-- 	end,
+-- }
