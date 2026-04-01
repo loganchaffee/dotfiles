7fbb75e0d03b75e9840ec8068e584b843ae7cb2b
Logan Chaffee <loganschaffee@gmail.com>
2026-03-31 22:43:40 -0700

    Require all plugins recursively

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
 require("settings")
 require("lsp")
-require("plugins.colorscheme")
+
+-- Require all plugins
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/plugins")) do
+  if file:match("%.lua$") then
+    require("plugins." .. file:gsub("%.lua$", ""))
+  end
+end
+
