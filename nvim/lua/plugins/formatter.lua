75d187eda37e183b45dd8fef16fcaedf8c98f068
Logan Chaffee <loganschaffee@gmail.com>
2026-04-04 13:26:39 -0700

    Add formatter

---
+++ nvim/lua/plugins/formatter.lua
@@ -0,0 +1,28 @@
+vim.pack.add({ "https://github.com/stevearc/conform.nvim.git" })
+
+require("conform").setup({
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		-- GraphQL = { "prettier" },
+		graphql = {},
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		c = { "clang-format" },
+		go = { "goimports", "gofmt" },
+		scala = {},
+	},
+	format_on_save = {
+		lsp_fallback = false,
+		async = false,
+		timeout_ms = 500,
+	},
+})
