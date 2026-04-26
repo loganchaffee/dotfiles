af045300110873c587d2129149e9837c434d8ae9
Logan Chaffee <loganschaffee@gmail.com>
2026-04-26 16:57:33 -0700

    Update treesitter parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,25 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter.git" })
 
 local installed_parsers = {
-  "javascript",
-  "typescript",
-  "tsx",
-  "css",
-  "scss",
-  "html",
-  "go",
-  "php",
-  "scala",
-  "bash",
-  "python",
-  "java",
+	"javascript",
+	"typescript",
+	"tsx",
+	"css",
+	"scss",
+	"html",
+	"go",
+	"php",
+	"scala",
+	"bash",
+	"python",
+	"java",
 }
 
 vim.api.nvim_create_autocmd("FileType", {
-  callback = function()
-    local ft = vim.bo.filetype
-    if vim.tbl_contains(installed_parsers, ft) then
-      pcall(vim.treesitter.start)
-    end
-  end,
+	callback = function()
+		local ft = vim.bo.filetype
+		if vim.tbl_contains(installed_parsers, ft) then
+			pcall(vim.treesitter.start)
+		end
+	end,
 })
